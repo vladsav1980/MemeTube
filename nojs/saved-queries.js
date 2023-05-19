ae0b0ee807cc5fb270f31ca47495c542d496ed3b
vladsav1980 <vladsav1980@gmail.com>
2023-05-19 22:36:31 +0300

    you can remove added searches

--- nojs/saved-queries.js
+++ nojs/saved-queries.js
@@ -1,51 +1,72 @@
-window.addEventListener('DOMContentLoaded', function() {
-  loadSavedQueries();
-});
+// Отримуємо елемент контейнера для збережених запитів
+var savedQueriesContainer = document.getElementById('saved-queries');
 
-function loadSavedQueries() {
-  var savedQueries = localStorage.getItem('savedQueries');
+// Завантажуємо збережені запити з локального сховища
+var savedQueries = JSON.parse(localStorage.getItem('savedQueries')) || [];
 
-  if (savedQueries) {
-    savedQueries = JSON.parse(savedQueries);
+// Завантажуємо збережені запити під час завантаження сторінки
+document.addEventListener('DOMContentLoaded', loadSavedQueries);
 
-    for (var i = 0; i < savedQueries.length; i++) {
-      var query = savedQueries[i];
+// Зберігаємо новий запит
+function saveQuery() {
+  var query = document.getElementById('search-query').value;
 
-      var savedQueryButton = document.createElement('button');
-      savedQueryButton.classList.add('saved-query-button');
-      savedQueryButton.innerText = query;
-      savedQueryButton.addEventListener('click', function() {
-        document.getElementById('search-query').value = query;
-        searchVideos();
-      });
+  if (query.trim() !== '') {
+    // Перевіряємо, чи запит вже збережений
+    if (!savedQueries.includes(query)) {
+      savedQueries.push(query);
+      localStorage.setItem('savedQueries', JSON.stringify(savedQueries));
 
+      // Створюємо кнопку для збереженого запиту
+      var savedQueryButton = createSavedQueryButton(query);
+
+      // Додаємо кнопку до контейнера збережених запитів
       savedQueriesContainer.appendChild(savedQueryButton);
     }
   }
 }
 
-function saveQuery() {
-  var query = document.getElementById('search-query').value;
+// Видаляємо збережений запит
+function removeSavedQuery(query) {
+  var queryIndex = savedQueries.indexOf(query);
 
-  if (query.trim() !== '') {
-    var savedQueries = localStorage.getItem('savedQueries');
-
-    if (savedQueries) {
-      savedQueries = JSON.parse(savedQueries);
-    } else {
-      savedQueries = [];
-    }
-
-    savedQueries.push(query);
+  if (queryIndex !== -1) {
+    savedQueries.splice(queryIndex, 1);
     localStorage.setItem('savedQueries', JSON.stringify(savedQueries));
+  }
+}
 
-    var savedQueryButton = document.createElement('button');
-    savedQueryButton.classList.add('saved-query-button');
-    savedQueryButton.innerText = query;
-    savedQueryButton.addEventListener('click', function() {
-      document.getElementById('search-query').value = query;
-      searchVideos();
-    });
+// Створюємо кнопку для збереженого запиту
+function createSavedQueryButton(query) {
+  var savedQueryButton = document.createElement('button');
+  savedQueryButton.classList.add('saved-query-button');
+  savedQueryButton.innerText = query;
+
+  var removeButton = document.createElement('button');
+  removeButton.classList.add('remove-button');
+  removeButton.innerText = 'x';
+  removeButton.addEventListener('click', function(event) {
+    event.stopPropagation();
+    removeSavedQuery(query);
+    savedQueriesContainer.removeChild(savedQueryButton);
+  });
+
+  savedQueryButton.appendChild(removeButton);
+
+  savedQueryButton.addEventListener('click', function() {
+    document.getElementById('search-query').value = query;
+    searchVideos();
+  });
+
+  return savedQueryButton;
+}
+
+// Завантажуємо збережені запити під час завантаження сторінки
+function loadSavedQueries() {
+  for (var i = 0; i < savedQueries.length; i++) {
+    var query = savedQueries[i];
+
+    var savedQueryButton = createSavedQueryButton(query);
 
     savedQueriesContainer.appendChild(savedQueryButton);
   }
